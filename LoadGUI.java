2e9bc2358407d13a49a118f4f723dcc5d63c5c4c
dungvvo <47492737+dungvvo@users.noreply.github.com>
2019-04-30 21:21:03 -0500

    Add files via upload

--- LoadGUI.java
+++ LoadGUI.java
@@ -9,44 +9,45 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class LoadGUI extends BorderPane {
-	
-	Label filePathText; // label for the input
-	TextField filePathInput; // input field for filepath to load
-	boolean validFilePath = true; // display "invalid input" if bad input
-	Text invalidInput = new Text("Invalid Input");
-	String filepath;
-	Button load;
-	Button returnHome;
 
-	public LoadGUI(Main main, Stage primaryStage) {
-		filePathText = new Label("Input filepath for load: ");
-		invalidInput.setVisible(false);
-		filePathInput = new TextField();
-		load = new Button("Load");
-		load.setOnAction(e -> {
-			try {
-				filepath = filePathInput.getText();
-				System.out.println("DEBUG: filepath: " + filepath);
-				main.allQuestions.loadQuestions(); // TODO loadQuestions needs file input from here
-				validFilePath = true; // if reached, no exceptions (go to home menu)
-			} catch (Exception exc) { // TODO more specific exception handling
-				System.out.println("DEBUG: exception in Load");
-				validFilePath = false; // exceptions come from parsing filepath or from filenotfound
-				invalidInput.setVisible(true);
-			}
-			if (validFilePath) {
-				primaryStage.setScene(main.home); // return home if input valid
-			}
-		});
-		returnHome = new Button("Return to Home");
-		returnHome.setOnAction(e -> {
-			primaryStage.setScene(main.home);
-			; // return home
-		});
-		
-		VBox filePathInputBox = new VBox(filePathText,filePathInput);
-		VBox centerBox = new VBox(filePathInputBox,load,invalidInput);
-		setCenter(centerBox);
-		setTop(returnHome);
-	}
+  Label filePathText; // label for the input
+  TextField filePathInput; // input field for filepath to load
+  boolean validFilePath = true; // display "invalid input" if bad input
+  Text invalidInput = new Text("Invalid Input");
+  String filepath;
+  Button load;
+  Button returnHome;
+
+  public LoadGUI(Main main, Stage primaryStage) {
+    filePathText = new Label("Input filepath for load: ");
+    invalidInput.setVisible(false);
+    filePathInput = new TextField();
+    load = new Button("Load");
+    load.setOnAction(e -> {
+      try {
+        filepath = filePathInput.getText();
+        main.allQuestions.loadQuestions(filepath); // TODO loadQuestions needs file input from here
+        validFilePath = true; // if reached, no exceptions (go to home menu)
+      } catch (Exception exc) { // TODO more specific exception handling
+        System.out.println("DEBUG: exception in Load");
+        exc.printStackTrace();
+        validFilePath = false; // exceptions come from parsing filepath or from filenotfound
+        invalidInput.setVisible(true);
+      }
+      if (validFilePath) {
+
+        primaryStage.setScene(main.home); // return home if input valid
+      }
+    });
+    returnHome = new Button("Return to Home");
+    returnHome.setOnAction(e -> {
+      primaryStage.setScene(main.home);
+      ; // return home
+    });
+
+    VBox filePathInputBox = new VBox(filePathText, filePathInput);
+    VBox centerBox = new VBox(filePathInputBox, load, invalidInput);
+    setCenter(centerBox);
+    setTop(returnHome);
+  }
 }
--- QuestionBank.java
+++ QuestionBank.java
@@ -17,134 +17,136 @@ import javafx.scene.image.Image;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
-
 /**
  * stores all the questions that have been added by the user
+ * 
  * @author Spencer
  *
  */
 public class QuestionBank {
-	private int totalNumQuestions;
-	private ArrayList<Question> questionBank;
+  private int totalNumQuestions;
+  private ArrayList<Question> questionBank;
 
-	public QuestionBank() {
-		totalNumQuestions = 0;
-		questionBank = new ArrayList<Question>();
-	}
-	
-	 /*
-	   * Load all the questions to the database
-	   */
-	  public void loadQuestions() throws IOException, ParseException {
-		  // TODO probably want a way to let the user specify the filepath
+  public QuestionBank() {
+    totalNumQuestions = 0;
+    questionBank = new ArrayList<Question>();
+  }
 
-	    System.out.println("start loading");
+  /*
+   * Load all the questions to the database
+   */
+  public void loadQuestions(String filepath) throws IOException, ParseException {
 
-	    File file = new File(
-	        "C:/Users/vvddu/Desktop/Wisconsin/Study/CompSci_Java_Course/QuizGenerator/application/questions_001.json");
-	    FileReader readFile = new FileReader(file);
-	    Object tempObj = new JSONParser().parse(readFile);
-	    JSONObject obj = (JSONObject) tempObj;
-	    JSONArray questions = (JSONArray) obj.get("questionArray");
-	    System.out.println("parse: " + questions.size());
+    System.out.println("start loading");
 
-	    for (int i = 0; i < questions.size(); i++) {
-	      JSONObject jsonQuestion = (JSONObject) questions.get(i);
-	      String questionText = (String) jsonQuestion.get("questionText");
-	      String questionTopic = (String) jsonQuestion.get("topic");
-	      String imageName = (String) jsonQuestion.get("image");
-	      BufferedImage image = null;
-	      if (!imageName.equals("none")) {
-	        File inputFile = new File(imageName); // image file path
-	        int width = 963; // width of the image
-	        int height = 640; // height of the image
-	        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-	        image = ImageIO.read(inputFile);
-	      }
-	      JSONArray questionChoices = (JSONArray) jsonQuestion.get("choiceArray");
-	      List<AnswerChoice> choices = new ArrayList<AnswerChoice>();
-	      for (int j = 0; j < questionChoices.size(); j++) {
-	        String currChoice = questionChoices.get(j).toString();
-	        String[] temp = currChoice.split(",");
-	        String[] isCorrect = temp[0].split(":");
-	        String[] choice = temp[1].split(":");
-	        isCorrect[1] = isCorrect[1].replace("\"", "");
-	        choice[1] = choice[1].replace("\"", "");
-	        boolean correctness = false;
-	        if (isCorrect[1].equals("T"))
-	          correctness = true;
-	        choices.add(new AnswerChoice(choice[1], correctness));
-	      }
-	      Question newQuestion = new Question(questionText, questionTopic, choices, image);
-	      questionBank.add(newQuestion);
-	      totalNumQuestions++;
-	    }
-	  }
+    File file = new File(filepath);
+    FileReader readFile = new FileReader(file);
+    Object tempObj = new JSONParser().parse(readFile);
+    JSONObject obj = (JSONObject) tempObj;
+    JSONArray questions = (JSONArray) obj.get("questionArray");
 
+    for (int i = 0; i < questions.size(); i++) {
+      JSONObject jsonQuestion = (JSONObject) questions.get(i);
+      String questionText = (String) jsonQuestion.get("questionText");
+      String questionTopic = (String) jsonQuestion.get("topic");
+      String imageName = (String) jsonQuestion.get("image");
+      BufferedImage image = null;
+      if (!imageName.equals("none")) {
+        File inputFile = new File(imageName); // image file path
+        int width = 963; // width of the image
+        int height = 640; // height of the image
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        image = ImageIO.read(inputFile);
+      }
 
-	/**
-	 * add a question to the question bank
-	 * @param newQuestion the question to be added
-	 */
-	public void addQuestion(Question newQuestion) {
-		// update the topics list in main 
-		if (newQuestion != null) {
-			questionBank.add(newQuestion);
-		}
-		totalNumQuestions++;
-	}
+      JSONArray questionChoices = (JSONArray) jsonQuestion.get("choiceArray");
+      List<AnswerChoice> choices = new ArrayList<AnswerChoice>();
+      for (int j = 0; j < questionChoices.size(); j++) {
+        String currChoice = questionChoices.get(j).toString();
+        String[] temp = currChoice.split("\",\"");
+        String[] isCorrect = temp[1].split(":");
+        String[] choice = temp[0].split(":");
+        isCorrect[1] = isCorrect[1].replace("\"", "");
 
-	/**
-	 * helper method to generate a quiz from the question bank
-	 * @param topic the topic that we want to find
-	 * @return an arraylist of the matching questions
-	 */
-	private ArrayList<Question> findAllQuestionsWithTopic(String topic) {
-		ArrayList<Question> ret = new ArrayList<Question>();
-		for (Question question : questionBank) {
-		  if (question.getTopic().equals(topic)) {
-		    ret.add(question);
-		  }
-		}
-		return ret;
-	}
+        choice[1] = choice[1].replace("\"", "");
+        boolean correctness = false;
+        if (isCorrect[1].equals("T"))
+          correctness = true;
+        choices.add(new AnswerChoice(choice[1], correctness));
+      }
+      Question newQuestion = new Question(questionText, questionTopic, choices, image);
+      questionBank.add(newQuestion);
+      totalNumQuestions++;
+    }
+  }
 
-	/**
-	 * randomly generate a quiz from the entire bank of questions
-	 * @param selectedTopics the topics that this quiz will be comprised of
-	 * @param numQuestions the number of questions in this quiz
-	 * @return a Quiz with numQuestions number of questions and topics that match selectedTopics
-	 */
-	public Quiz generateQuiz(HashSet<String> selectedTopics, int numQuestions) {
-		String[] topicslist = selectedTopics.toArray(new String[0]);
-		ArrayList<Question> matchingQuestions = new ArrayList<Question>();
-		for (int i = 0; i < topicslist.length; ++i) {
-			matchingQuestions.addAll(findAllQuestionsWithTopic(topicslist[i]));
-		}
+  /**
+   * add a question to the question bank
+   * 
+   * @param newQuestion the question to be added
+   */
+  public void addQuestion(Question newQuestion) {
+    // update the topics list in main
+    if (newQuestion != null) {
+      questionBank.add(newQuestion);
+    }
+    totalNumQuestions++;
+  }
 
-	    if (matchingQuestions.size() < numQuestions) {
-	      return null; // Invalid number of questions
-	    }
-	    
-		Collections.shuffle(matchingQuestions); // randomize
-		Quiz ret = new Quiz(matchingQuestions.subList(0,numQuestions)); // construct quiz
-		return ret;
-	}
-	
-	public ArrayList<Question> getQuestionBank() {
-		return questionBank;
-	}
-	
-	public int getTotalNumQuestions() {
-		return totalNumQuestions;
-	}
-	
-	/**
-	 * given a file path, save all the questions in this question bank to a json file at the specified
-	 * location
-	 * @param filepath string representation of the file path to be used
-	 */
-	public void saveQuestionsToFile(String filepath) {
-		// TODO implement
-	}
+  /**
+   * helper method to generate a quiz from the question bank
+   * 
+   * @param topic the topic that we want to find
+   * @return an arraylist of the matching questions
+   */
+  private ArrayList<Question> findAllQuestionsWithTopic(String topic) {
+    ArrayList<Question> ret = new ArrayList<Question>();
+    for (Question question : questionBank) {
+      if (question.getTopic().equals(topic)) {
+        ret.add(question);
+      }
+    }
+    return ret;
+  }
+
+  /**
+   * randomly generate a quiz from the entire bank of questions
+   * 
+   * @param selectedTopics the topics that this quiz will be comprised of
+   * @param numQuestions   the number of questions in this quiz
+   * @return a Quiz with numQuestions number of questions and topics that match selectedTopics
+   */
+  public Quiz generateQuiz(HashSet<String> selectedTopics, int numQuestions) {
+    String[] topicslist = selectedTopics.toArray(new String[0]);
+    ArrayList<Question> matchingQuestions = new ArrayList<Question>();
+    for (int i = 0; i < topicslist.length; ++i) {
+      matchingQuestions.addAll(findAllQuestionsWithTopic(topicslist[i]));
+    }
+
+    if (matchingQuestions.size() < numQuestions) {
+      return null; // Invalid number of questions
+    }
+
+    Collections.shuffle(matchingQuestions); // randomize
+    Quiz ret = new Quiz(matchingQuestions.subList(0, numQuestions)); // construct quiz
+    return ret;
+  }
+
+  public ArrayList<Question> getQuestionBank() {
+    return questionBank;
+  }
+
+  public int getTotalNumQuestions() {
+    return totalNumQuestions;
+  }
+
+  /**
+   * given a file path, save all the questions in this question bank to a json file at the specified
+   * location
+   * 
+   * @param filepath string representation of the file path to be used
+   */
+  public void saveQuestionsToFile(String filepath) {
+    // TODO implement
+  }
 }
