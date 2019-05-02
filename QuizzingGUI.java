34b0c8b2e2ef4097845f969cdd7a2a2bceb8563c
kkim6109 <46933251+kkim6109@users.noreply.github.com>
2019-05-02 18:13:49 -0500

    Add files via upload

--- QuizzingGUI.java
+++ QuizzingGUI.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
@@ -19,155 +20,163 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
- * Filename: QuizzingGUI.java Project: A-Team Quiz Application Team: A-Team 27
- * Authors: Nicholas Havlicek, Murad Jaber, Kevin Kim, Spencer Runde, Dung Vo
+ * Filename: QuizzingGUI.java Project: A-Team Quiz Application Team: A-Team 27 Authors: Nicholas
+ * Havlicek, Murad Jaber, Kevin Kim, Spencer Runde, Dung Vo
  * 
  * GUI which quizzes the user
  */
 public class QuizzingGUI extends BorderPane {
-	int currentQuestionNumber = 1;
-	Text previousQuestionCorrect;
-	Text previousQuestionIncorrect;
-	Button submitAnswer;
-	Label questionBodyLabel;
-	Label questionTopicLabel;
-	RadioButton[] choiceIsCorrect;
-	Label[] choiceText;
-	ToggleGroup tg;
-	ImageView questionImageLocation;
-	Image questionImage;
+  int currentQuestionNumber = 1;
+  Text previousQuestionCorrect;
+  Text previousQuestionIncorrect;
+  Button submitAnswer;
+  Label questionBodyLabel;
+  Label questionTopicLabel;
+  RadioButton[] choiceIsCorrect;
+  Label[] choiceText;
+  ToggleGroup tg;
+  ImageView questionImageLocation;
+  Image questionImage;
 
-	/**
-	 * Sets up the GUI for Quizzing screen
-	 * 
-	 * @param main         Instance of the Main class which controls the GUIs and is
-	 *                     the "Main" class
-	 * @param primaryStage The primary stage for the main GUI
-	 */
-	public QuizzingGUI(Main main, Stage primaryStage) {
+  /**
+   * Sets up the GUI for Quizzing screen
+   * 
+   * @param main         Instance of the Main class which controls the GUIs and is the "Main" class
+   * @param primaryStage The primary stage for the main GUI
+   */
+  public QuizzingGUI(Main main, Stage primaryStage) {
 
-		// return home button
-		VBox topSide = new VBox(0);
-		topSide.setPadding(main.buttonSpacing);
-		
-		// previous question correctness
-		previousQuestionCorrect = new Text("Answer was correct!");
-		previousQuestionCorrect.setFill(Color.GREEN);
-		previousQuestionIncorrect = new Text("Answer was incorrect.");
-		previousQuestionIncorrect.setFill(Color.RED);
-		previousQuestionCorrect.setVisible(false);
-		previousQuestionIncorrect.setVisible(false);
+    // return home button
+    VBox topSide = new VBox(0);
+    topSide.setPadding(main.buttonSpacing);
 
-		// question info input
-		questionImageLocation = new ImageView();
-		questionImageLocation.setFitHeight(200);
-		questionImageLocation.setFitWidth(200);
-		questionBodyLabel = new Label("Question " + currentQuestionNumber + ": ");
-		questionTopicLabel = new Label("Topic: ");
-		
-		topSide.getChildren().add(questionImageLocation);
-		topSide.getChildren().add(questionBodyLabel);
-		topSide.getChildren().add(questionTopicLabel);
-		topSide.getChildren().add(previousQuestionCorrect);
-		topSide.getChildren().add(previousQuestionIncorrect);
+    // previous question correctness
+    previousQuestionCorrect = new Text("Previous answer was correct!");
+    previousQuestionCorrect.setFill(Color.GREEN);
+    previousQuestionIncorrect = new Text("Previous answer was incorrect.");
+    previousQuestionIncorrect.setFill(Color.RED);
+    previousQuestionCorrect.setVisible(false);
+    previousQuestionIncorrect.setVisible(false);
 
-		// question options
-		VBox midPart = new VBox(5);
-		midPart.setPadding(main.buttonSpacing);
+    // question info input
+    questionImageLocation = new ImageView();
+    questionImageLocation.setFitHeight(200);
+    questionImageLocation.setFitWidth(200);
+    questionBodyLabel = new Label("Question " + currentQuestionNumber + ": ");
+    questionTopicLabel = new Label("Topic: ");
 
-		Label selectRightAnswer = new Label("Select Correct Answer: ");
-		midPart.getChildren().add(selectRightAnswer);
+    topSide.getChildren().add(questionImageLocation);
+    topSide.getChildren().add(questionBodyLabel);
+    topSide.getChildren().add(questionTopicLabel);
+    topSide.getChildren().add(previousQuestionCorrect);
+    topSide.getChildren().add(previousQuestionIncorrect);
 
-		tg = new ToggleGroup();
-		choiceIsCorrect = new RadioButton[5];
-		choiceText = new Label[5];
-		for (int i = 0; i < 5; i++) {
-			HBox questionRow = new HBox(10);
-			choiceText[i] = new Label("Choice " + (i + 1));
-			choiceIsCorrect[i] = new RadioButton();
+    // question options
+    VBox midPart = new VBox(5);
+    midPart.setPadding(main.buttonSpacing);
 
-			choiceIsCorrect[i].setId(i + ""); // Saves which answer it's associated with
-			choiceIsCorrect[i].setToggleGroup(tg);
+    Label selectRightAnswer = new Label("Select Correct Answer: ");
+    midPart.getChildren().add(selectRightAnswer);
 
-			questionRow.getChildren().add(choiceText[i]);
-			questionRow.getChildren().add(choiceIsCorrect[i]);
+    tg = new ToggleGroup();
+    choiceIsCorrect = new RadioButton[5];
+    choiceText = new Label[5];
+    for (int i = 0; i < 5; i++) {
+      HBox questionRow = new HBox(10);
+      choiceText[i] = new Label("Choice " + (i + 1));
+      choiceIsCorrect[i] = new RadioButton();
 
-			midPart.getChildren().add(questionRow);
-		}
+      choiceIsCorrect[i].setId(i + ""); // Saves which answer it's associated with
+      choiceIsCorrect[i].setToggleGroup(tg);
 
-		// submit question button
-		submitAnswer = new Button("Submit");
-		submitAnswer.setOnAction(e -> {
-			AnswerChoice answer = getAnswerChoice(main);
-			if (answer != null) {
-				if (main.currQuiz.checkAnswer(answer)) {
-					main.currQuiz.numCorrect++;
-					previousQuestionCorrect.setVisible(true);
-					previousQuestionIncorrect.setVisible(false);
-				} else {
-					previousQuestionCorrect.setVisible(false);
-					previousQuestionIncorrect.setVisible(true);
-				}
-				if (main.currQuiz.quizOver()) {
-					Alert totalScore = new Alert(AlertType.INFORMATION);
-					totalScore.setTitle("Total Score");
-					totalScore.setContentText("You got " + main.currQuiz.numCorrect + 
-							" questions correct out of " + main.currQuiz.numQuestions + " possible ("
-									+ (int) main.currQuiz.numCorrect / main.currQuiz.numQuestions*100 + "%)");
-					totalScore.showAndWait();
-					primaryStage.setScene(main.home);
-					previousQuestionCorrect.setVisible(false);
-					previousQuestionIncorrect.setVisible(false);
-				} else {
-					updateFields(main.currQuiz.getNextQuestion());
-				}
-			}
-		});
+      questionRow.getChildren().add(choiceText[i]);
+      questionRow.getChildren().add(choiceIsCorrect[i]);
 
-		setTop(topSide);
-		setCenter(midPart);
-		setBottom(submitAnswer);
+      midPart.getChildren().add(questionRow);
+    }
 
-		// TODO implement
-		// 4. display option for an image
+    // submit question button
+    submitAnswer = new Button("Submit");
+    submitAnswer.setOnAction(e -> {
+      AnswerChoice answer = getAnswerChoice(main);
+      if (answer != null) {
+        if (main.currQuiz.checkAnswer(answer)) {
+          main.currQuiz.numCorrect++;
+          previousQuestionCorrect.setVisible(true);
+          previousQuestionIncorrect.setVisible(false);
+        } else {
+          previousQuestionCorrect.setVisible(false);
+          previousQuestionIncorrect.setVisible(true);
+        }
+        if (main.currQuiz.quizOver()) {
+          previousQuestionCorrect.setVisible(false);
+          previousQuestionIncorrect.setVisible(false);
+          Alert totalScore = new Alert(AlertType.INFORMATION);
+          totalScore.setTitle("Total Score");
+          totalScore.setContentText("You got " + main.currQuiz.numCorrect
+              + " questions correct out of " + main.currQuiz.numQuestions + " possible ("
+              + (int) main.currQuiz.numCorrect / main.currQuiz.numQuestions * 100 + "%)");
+          totalScore.showAndWait();
+          primaryStage.setScene(main.home);
+        } else {
+          updateFields(main.currQuiz.getNextQuestion());
+        }
+      }
+    });
 
-	}
+    setTop(topSide);
+    setCenter(midPart);
+    setBottom(submitAnswer);
 
-	private AnswerChoice getAnswerChoice(Main main) {
-		RadioButton button;
-		Quiz quiz = main.currQuiz;
-		Question question = quiz.getCurrQuestion();
+    // TODO implement
+    // 4. display option for an image
 
-		if (tg.getSelectedToggle() != null) {
-			button = (RadioButton) tg.getSelectedToggle();
-			try {
-				int choice = Integer.parseInt(button.getId());
-				return question.getChoices().get(choice);
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				return null;
-			}
-		}
-		return null;
-	}
+  }
 
-	public void updateFields(Question currQuestion) {
-		// update the fields (e.g. choice1Text, question body, etc);
-		questionBodyLabel.setText("Question: " + currQuestion.getQuestionText()); // Make These to get methods
-		questionTopicLabel.setText("Topic: " + currQuestion.getTopic());
-		currentQuestionNumber++; // increment the display for the current question upon advancing questions
+  private AnswerChoice getAnswerChoice(Main main) {
+    RadioButton button;
+    Quiz quiz = main.currQuiz;
+    Question question = quiz.getCurrQuestion();
 
-		if (tg.getSelectedToggle() != null) {
-		  tg.getSelectedToggle().setSelected(false);
-		}
-		
-		for (int i = 0; i < choiceIsCorrect.length; i++) {
-			choiceText[i].setText(currQuestion.getChoices().get(i).getChoiceText());// Use getter
-			choiceIsCorrect[i] = new RadioButton();
+    if (tg.getSelectedToggle() != null) {
+      button = (RadioButton) tg.getSelectedToggle();
+      try {
+        int choice = Integer.parseInt(button.getId());
+        return question.getChoices().get(choice);
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+        return null;
+      }
+    }
+    return null;
+  }
 
-			choiceIsCorrect[i].setId(i + ""); // Saves which answer it's associated with
-			choiceIsCorrect[i].setToggleGroup(tg);
-		}
-	}
+  public void updateFields(Question currQuestion) {
+    // update the fields (e.g. choice1Text, question body, etc);
+    if (currQuestion.getImage() != null) {
+      questionImageLocation.setImage(SwingFXUtils.toFXImage(currQuestion.getImage(), null));
+    }
+    questionBodyLabel.setText("Question: " + currQuestion.getQuestionText()); // Make These to get
+                                                                              // methods
+    questionTopicLabel.setText("Topic: " + currQuestion.getTopic());
+    currentQuestionNumber++; // increment the display for the current question upon advancing
+                             // questions
+
+    if (tg.getSelectedToggle() != null) {
+      tg.getSelectedToggle().setSelected(false);
+    }
+
+    for (int i = 0; i < choiceIsCorrect.length; i++) {
+      if (i < currQuestion.getChoices().size()) {
+        choiceText[i].setText(currQuestion.getChoices().get(i).getChoiceText());
+      } else {
+        choiceText[i].setText("");
+      }
+      choiceIsCorrect[i] = new RadioButton();
+
+      choiceIsCorrect[i].setId(i + ""); // Saves which answer it's associated with
+      choiceIsCorrect[i].setToggleGroup(tg);
+    }
+  }
 
 }
